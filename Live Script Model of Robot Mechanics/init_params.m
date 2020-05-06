7d9a1e5457ac85c3443b2e8be65eff82860ff722
Michael Rencheck <rencheck.1@live.com>
2020-05-06 16:23:00 -0500

    Updated Bike params to match CAD

--- Live Script Model of Robot Mechanics/init_params.m
+++ Live Script Model of Robot Mechanics/init_params.m
@@ -4,7 +4,7 @@
 %   Initializes the values of many parameters, such as parameters in the
 %   system dynamics, parameters that relate to simulating the system
 %   forward in time, and parametes that relate to visualization/animation.
-%   
+%
 % Inputs:
 %   none
 
@@ -14,27 +14,30 @@
 function params = init_params
     % parameters that appear in the dynamics:
      params.model.dyn.com.m = 5;    % mass of the bike body
-     params.model.dyn.bw.m = 0.2;    % mass of bw
-     params.model.dyn.fw.m = 0.2;    % mass of fw
-     
-     params.model.dyn.bw.I = 0.001;% moment of inertia of bw
-     params.model.dyn.fw.I = 0.001; % moment of inertia of fw
-     params.model.dyn.com.I = 0.05; % moment of inertia of com
-     
+     params.model.dyn.bw.m = 0.185;    % mass of bw
+     params.model.dyn.fw.m = 0.185;    % mass of fw
+
+     params.model.dyn.bw.I = 0.000380; % moment of inertia of bw
+     params.model.dyn.fw.I = 0.000380; % moment of inertia of fw
+     params.model.dyn.com.I = 0.1225; % moment of inertia of com
+
      params.model.dyn.g = 9.81;      % acceleration due to gravity
-     params.model.dyn.tau_bw = 0;      % acceleration due to gravity
-    
+     params.model.dyn.tau_bw = 0;    % applied torque from the backwheel
+
     % parameters that help with visualizing the robot:
     params.model.geom.body.w = 0.5; % width of the bike body
     params.model.geom.body.h = 0.5; % height of the bike body
     params.model.geom.leg.l = 0.5;   % length of the bike legs
     params.model.geom.leg.w = 0.1; % width of the bike legs
-    params.model.geom.wheel.r = 0.1; % radius of wheels 
-    params.model.geom.bw_com.l = 1.5;% straight line distance between back wheel and CoM
-    params.model.geom.bw_com.theta = pi/4; %Initial angle between back wheel and CoM
-    params.model.geom.fw_com.l = 1.5;% straight line distance between back wheel and CoM
-    params.model.geom.fw_com.theta = 3*pi/4; %Initial angle between back wheel and CoM
-    
+
+    params.model.geom.wheel.r = 0.0625; % radius of wheels
+
+    params.model.geom.bw_com.l = 0.22757; % straight line distance between back wheel and CoM
+    params.model.geom.bw_com.theta = 1.101; %Initial angle between back wheel and CoM
+
+    params.model.geom.fw_com.l = 0.31584;% straight line distance between back wheel and CoM
+    params.model.geom.fw_com.theta = 2.464; %Initial angle between back wheel and CoM
+
     params.viz.colors.body = [0.5 0.5 0.5];
     params.viz.colors.leg = [0.25 0.25 0.25];
     params.viz.colors.wheels = [0.75 0.75 0.75];
@@ -42,7 +45,7 @@ function params = init_params
     params.viz.colors.tracers.leg = 'g';
     params.viz.colors.tracers.wheels = 'b';
     params.viz.axis_lims = [-3,3,-2,2];
-    
+
     % parameters related to simulating (integrating) the dynamics forward
     % in time:
     params.sim.ICs.x_com = 0;      % initial positions
@@ -54,6 +57,6 @@ function params = init_params
     params.sim.ICs.dy_com = 0;
     params.sim.ICs.dtheta_com = 0;
     params.sim.ICs.dtheta_fw = 0;
-    params.sim.ICs.dtheta_bw = 0; 
+    params.sim.ICs.dtheta_bw = 0;
     params.sim.dt = 0.05;           % simulation timestep
 end
