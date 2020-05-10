5b85fb42935260de8df44e9ce2a2f47e8fec7ae5
Hay <suntry2020@u.northwestern.edu>
2020-05-10 17:39:11 -0500

    ramp constraints

--- Live Script Model of Robot Mechanics/constraint_derivatives.m
+++ Live Script Model of Robot Mechanics/constraint_derivatives.m
@@ -23,10 +23,15 @@ theta_com = x(3);
 r_bw = params.model.geom.wheel.r;
 r_fw = params.model.geom.wheel.r;
 bw_fw_distance = params.model.geom.bw_fw.l;
+x_ramp = params.model.geom.ramp.x;
+y_ramp = params.model.geom.ramp.y;
+
+x_bf = x(1);
+y_bf = x(2);
 
 
-[A_all,H_cbw_x,H_cbw_y,H_cfw_y, H_cfw_bw] = autogen_constraint_derivatives(bw_fw_distance,r_bw,r_fw,theta_com);
+[A_all,H_cbw_x,H_cbw_y,H_cfw_y, H_cfw_bw, H_fw_ramp, H_bw_ramp] = autogen_constraint_derivatives(bw_fw_distance,r_bw,r_fw,theta_com,x_bf,x_ramp,y_bf,y_ramp);
 %AUTOGEN_CONSTRAINT_DERIVATIVES);
-Hessian = cat(3,H_cbw_x,H_cbw_y,H_cfw_y,H_cfw_bw);
+Hessian = cat(3,H_cbw_x,H_cbw_y,H_cfw_y,H_cfw_bw,H_fw_ramp,H_bw_ramp);
 
 end
