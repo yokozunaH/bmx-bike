b3f09ac6cb83ce695209b80b2bd70c815b3a12f5
xinranli0809 <60499885+xinranli0809@users.noreply.github.com>
2020-06-07 21:35:48 -0500

    Adding comments
    Comments adding in the code.

--- Hardware Motor Control/main.c
+++ Hardware Motor Control/main.c
@@ -37,21 +37,7 @@
 #include "adcInit.h"
 #include "estop.h"
 
-
-//void ISR(void)
-//{
-//    uint32_t status=0;
-//    status = TimerIntStatus(TIMER5_BASE,true);
-//    TimerIntClear(TIMER5_BASE,status);
-////    if (flag==1)
-////    {
-////        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, pwmOut-60);
-////        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, pwmOut-60);
-////    }
-//
-//
-//}
-
+// An example ISR to demo that the ESTOP ISR has higher priority.
 void blueISR(void)
 {
     uint32_t status=0;
@@ -60,9 +46,9 @@ void blueISR(void)
 
     if (ledFlag == 1)
     {
+        // Turn on the blue LED
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
     }
-
 }
 
 
@@ -74,11 +60,11 @@ int main(void)
 
   // Set the clocking to run directly from the crystal.
   SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
-  // Enable the GPIO pins for the LED (PF2 & PF3).
-     //
+
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+  // Configure all the necessary peripherals.
   ConfigureUART();
   ConfigureI2C();
   ConfigureADC();
@@ -86,42 +72,37 @@ int main(void)
   ConfigureESTOP();
   ConfigureQEI();
   ConfigureQEIVel();
-//  ConfigureTimerAndInterrupt(triangle,ISR);
 
   ConfigureTimerAndInterrupt(blueISR);
-
   SysCtlDelay(SysCtlClockGet() / 60); //50ms
 
-//  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-
+  // Flag for the triangle wave generation interrupt, you can ignore it for this assignment
   flag=1;
   while(1)
   {
   //ledFlag = 1 means ESTOP ASSERTED
   if (ledFlag == 1)
     {
+        // Turn off motor and turn on red LED.
+        // These happens when the ESTOP is asserted.
+        // Since when this assignment was presented, the actual hardware setup of the relay was not yet realized, the ESTOP was then only implemented with software.
+        // In reality, ESTOP flag should turn on or off certain GPIO to then control some MOSFET to turn on or off the relay to shut off the power supply.
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
         PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, 4688);
         PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, 4688);
     }
   else
-  {
-      SysCtlDelay(SysCtlClockGet() / 60); //50ms
-  //      delayMs(1000);
-      struct sensors s = getADCValue();
-      UARTprintf("\rManual input: %d   ",s.POT);
-      UARTprintf("Position reading: %d   ",QEIPositionGet(QEI0_BASE));
-      UARTprintf("Velocity reading: %d   ",QEIVelocityGet(QEI0_BASE));
-      PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, s.POT);
-      PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, s.POT);
-  }
-//    // Turn on the LED.
-//    //
-//    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-//    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
-
+    {
+        // Driving the motor base on the potentiometer input and display the encoder readings.
+        SysCtlDelay(SysCtlClockGet() / 60); //50ms
+        struct sensors s = getADCValue();
+        UARTprintf("\rManual input: %d   ",s.POT);
+        UARTprintf("Position reading: %d   ",QEIPositionGet(QEI0_BASE));
+        UARTprintf("Velocity reading: %d   ",QEIVelocityGet(QEI0_BASE));
+        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, s.POT);
+        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_3, s.POT);
+    }
 
   }
 
-//  return 0;
 }
